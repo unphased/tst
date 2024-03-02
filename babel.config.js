2b9a7332b379666caf16816da48dc76487f37c0a
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-03-02 23:20:18 +0700

    hokay

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+export default {
+  "presets": [
+    ["@babel/preset-env", {
+      "targets": {
+        "node": "16"
+      },
+      "modules": false,
+      "debug": true,
+    }],
+    "@babel/preset-typescript"
+  ],
+  "ignore": [ "src/**/payload" ],
+  "highlightCode": true,
+  "sourceMaps": "inline",
+  "plugins": [
+  ]
+};
