f27aa05199715048edb5536161e05a9656602c82
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-04-19 12:49:43 +0700

    - old bugs fixed - new bugs added

--- src/tests/plotting.ts
+++ src/tests/plotting.ts
@@ -1,5 +1,12 @@
 import { test } from "../main.js";
 
-export const plot_vega_simple = test('vega_lite', ({ plot, a: {eq} }) => {
-  plot('vega_example', [{ title: 'test', data: [{a: 1, b: 2}, {a: 2, b: 2.5}]}]);
+export const most_basic_plot_vega = test('vega_lite', ({ plot, a: {eq} }) => {
+  plot('vega_example', [{
+    title: 'test',
+    data: [
+      {a: 1, b: 2},
+      {a: 1.2, b: 2},
+      {a: 2, b: 2.5}
+    ]
+  }]);
 });
