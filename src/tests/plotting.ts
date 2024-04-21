b679d3ab089d04142302d85a298681fa27fb9d4d
Steven Lu (MBP M1 Max) <stevenlu443@gmail.com>
2024-04-21 13:37:46 +0700

    yeah

--- src/tests/plotting.ts
+++ src/tests/plotting.ts
@@ -1,12 +1,13 @@
 import { test } from "../main.js";
 
 export const most_basic_plot_vega = test('vega_lite', ({ plot, a: {eq} }) => {
-  plot('vega_example', [{
+  plot('vega_example', {
     title: 'test',
     data: [
       {a: 1, b: 2},
       {a: 1.2, b: 2},
-      {a: 2, b: 2.5}
+      {a: 2, b: 2.5},
+      {a: 3, b: 3.5}
     ]
-  }]);
+  });
 });
