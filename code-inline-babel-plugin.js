e85d7580d773351ea6497ea4052b84ba5beebe8f
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-04-22 21:21:57 +0700

    - old bugs fixed - new bugs added

--- code-inline-babel-plugin.js
+++ code-inline-babel-plugin.js
@@ -1,12 +1,24 @@
-import * as types from '@babel/types';
+import * as util from 'util';
+function findFirstIdentifierByRegex(path, regex) {
+  let matchingNode = null;
+  path.traverse({
+    Identifier(subPath) {
+      if (regex.test(subPath.node.name)) {
+        matchingNode = subPath;
+        subPath.stop(); // Stop traversal as soon as we find the first match
+      }
+    }
+  });
+  return matchingNode;
+}
 
 export default function(_babel) {
-  const t = types;
+  const t = _babel.types;
 
   return {
     name: "inlining calls to grab code from source",
     visitor: {
-      CallExpression(path) {
+      CallExpression(path, state) {
         const callee = path.node.callee;
         if (
           callee.type === 'MemberExpression' &&
@@ -15,26 +27,11 @@ export default function(_babel) {
         ) {
           const objectName = callee.object.name;
           const arg = findFirstIdentifierByRegex(path, /__dirname\d*/);
-          console.warn(`Found readFileSync call on`, objectName, arg, path.node.loc.start);
+          console.warn(`Found readFileSync call on`, objectName, arg.node.loc.start, path.node.loc.start);
+          console.warn('filepath', state.file.opts.filename, 'path derived from siblings', util.inspect(arg.container.filter(c => c.type === 'StringLiteral').map(c => c.value), { colors: true, depth: 10 }));
         }
       },
     }
   }
 }
 
-
-function findFirstIdentifierByRegex(path, regex) {
-  let matchingNode = null;
-
-  // Here, we use `path.traverse` which automatically handles the context correctly
-  path.traverse({
-    Identifier(subPath) {
-      if (regex.test(subPath.node.name)) {
-        matchingNode = subPath.node;
-        subPath.stop(); // Stop traversal as soon as we find the first match
-      }
-    }
-  });
-
-  return matchingNode;
-}
