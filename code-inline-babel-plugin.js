8c2c584854380252aa7aba900e3f178245a2e2a5
Steven Lu (5950X 20.04[tmux]) <stevenlu443@gmail.com>
2024-04-22 06:31:18 -0400

    works

--- code-inline-babel-plugin.js
+++ code-inline-babel-plugin.js
@@ -1,4 +1,6 @@
 import * as types from '@babel/types';
+import traverse from '@babel/traverse';
+console.warn('traverse', traverse);
 
 export default function(_babel) {
   const t = types;
@@ -14,11 +16,27 @@ export default function(_babel) {
           callee.property.name === 'readFileSync'
         ) {
           const objectName = callee.object.name;
-          const filename = path.node.arguments[0];
-          console.warn(`Found readFileSync call on`, objectName, filename, path.node.loc.start);
+          const arg = findFirstIdentifierByRegex(path, /__dirname\d*/);
+          console.warn(`Found readFileSync call on`, objectName, arg, path.node.loc.start);
         }
       },
     }
   }
 }
 
+
+function findFirstIdentifierByRegex(path, regex) {
+  let matchingNode = null;
+
+  // Here, we use `path.traverse` which automatically handles the context correctly
+  path.traverse({
+    Identifier(subPath) {
+      if (regex.test(subPath.node.name)) {
+        matchingNode = subPath.node;
+        subPath.stop(); // Stop traversal as soon as we find the first match
+      }
+    }
+  });
+
+  return matchingNode;
+}
