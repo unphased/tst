db14eb9d3ec78125f453bcbab6ca2f7bf7f0c6a6
Steven Lu @ MBP-M1-Max <1542910+unphased@users.noreply.github.com>
2024-10-08 12:13:20 -0400

    okeydokes!

--- src/web-server.ts
+++ src/web-server.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import express from 'express';
+import { getLocalLANIPAddress } from 'ts-utils/network';
 
 // test results will be transformed from the standard test output format (which is a json structure and will in future
 // be temporally tree-diffable) into webpages. This can happen on demand or automatically.
@@ -22,58 +23,6 @@ export const pushTestResultPage = (name: string, content: string) => {
 export const clearTestResultPages = () => {
   testResultPages.clear();
 };
-
-import * as os from 'os';
-import { execSync } from 'child_process';
-
-function getDefaultInterface() {
-  let defaultInterface = '';
-
-  try {
-    if (os.platform() === 'darwin') {
-      // macOS
-      defaultInterface = execSync('route -n get default | grep interface | awk \'{print $2}\'', {
-        encoding: 'utf8',
-      }).trim();
-    } else if (os.platform() === 'linux') {
-      // Linux
-      const routeOutput = execSync('ip route', { encoding: 'utf8' });
-      const routes = routeOutput.split('\n');
-
-      let lowestMetric = Infinity;
-      for (const route of routes) {
-        if (route.includes('default')) {
-          const metric = parseInt(route.split('metric ')[1].trim(), 10);
-          if (metric < lowestMetric) {
-            lowestMetric = metric;
-            defaultInterface = route.split(' ')[4];
-          }
-        }
-      }
-    }
-  } catch (error) {
-    console.error('Error retrieving default interface:', error);
-  }
-
-  return defaultInterface;
-}
-
-function getLocalLANIPAddress() {
-  const defaultInterface = getDefaultInterface();
-  const interfaces = os.networkInterfaces();
-
-  if (defaultInterface && interfaces[defaultInterface]) {
-    const iface = interfaces[defaultInterface];
-
-    for (const alias of iface) {
-      if (alias.family === 'IPv4' && !alias.internal) {
-        return alias.address;
-      }
-    }
-  }
-
-  return null;
-}
 
 export function startServer(port = 4000) {
   // Set up Express
